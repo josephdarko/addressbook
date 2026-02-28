bb6c9c6df375f55dae37e3f9d26bf4a4f3de2a8e
josephdarko <151564452+josephdarko@users.noreply.github.com>
2026-02-28 17:53:42 +0000

    changes to UI pages

--- lib/contacts_page.dart
+++ lib/contacts_page.dart
@@ -15,9 +15,65 @@ super.key,
     ),
 body: Column(
   children: [
-    Padding(padding: EdgeInsets.symmetric(vertical: 30, horizontal: 15)
-
+    Padding(padding: EdgeInsets.symmetric(vertical: 30, horizontal: 15),
+        child: Container(
+          width: 45,
+          height: 30,
+          decoration: BoxDecoration(
+            color: Colors.grey,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Icon(Icons.person),
+              SizedBox(width: 10,), // to give space between text and icons
+              Text("First Contact", style: TextStyle(fontSize: 20) ,),
+            ],
+          ),
+        ),
+      
     ),
+    SizedBox(height: 10,),
+    
+    Padding(padding: EdgeInsets.symmetric(vertical: 20, horizontal: 15),
+      child: Container(
+        width: 45,
+        height: 30,
+        decoration: BoxDecoration(
+          color: Colors.grey,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Icon(Icons.person),
+            SizedBox(width: 10,), // to give space between text and icons
+            Text("Second Contact", style: TextStyle(fontSize: 20) ,),
+          ],
+        ),
+      ),
+    ),
+    SizedBox(height: 10,),
+
+    Padding(padding: EdgeInsets.symmetric(vertical: 20, horizontal: 15),
+      child: Container(
+        width: 45,
+        height: 30,
+        decoration: BoxDecoration(
+          color: Colors.grey,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Icon(Icons.person),
+            SizedBox(width: 10,), // to give space between text and icons
+            Text("Second Contact", style: TextStyle(fontSize: 20) ,),
+          ],
+        ),
+      ),
+    )
   ],
 ),
   );
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:projects/main.dart';
+import 'package:projects/settings_page.dart';
+
+
 
 
 class HomePage extends StatefulWidget {
@@ -29,121 +32,17 @@ class _HomePageState extends State<HomePage> {
       ),
 
 
-      body:Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-
-        children: [
-
-          Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-          child:Container(
-            width: double.infinity,
-            height: 200,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10),
-            ),
-
-            padding: EdgeInsets.all(16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                      Text("Quick Start",
-                        style: TextStyle(fontSize: 25),
-                      ),
-                SizedBox(height: 10,),
-
-                Text("Manage your contacts efficiently with our address book app",
-                style: TextStyle(fontSize: 18),),
-                SizedBox(height: 15,),
-
-                Container(
-                  height: 35,
-                  width: 200,
-                  decoration: BoxDecoration(
-                    color: Colors.grey,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Padding(padding:
-                  EdgeInsets.symmetric( horizontal: 12),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Icon(Icons.add,
-                        color: Colors.black,),
-
-                      Text("Add Contacts",
-                      style: TextStyle(color: Colors.black,
-                      fontSize: 16
-                      ),
-                      ),
-                    ],
-                  ),
-                  ),
-
-                )
-
-
-              ],
-            ),
 
 
 
-          ),
-    ),
-          SizedBox(height: 100,),
-          // features box
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20) ,
-          child: Container(
-            width: double.infinity,
-            height: 200,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10),
-            ),
-
-           padding: EdgeInsets.all(16),//in the container
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start, // children to the side
-              children: [
-                // create n manage contacts
-                Row(
-                  children: [
-                    Icon(Icons.check, color: Colors.greenAccent,),
-                    SizedBox(width: 10,) ,// space between the the icon and text
-                    Text("Create and Manage Contacts",
-                    style: TextStyle(fontSize: 20),),
-                  ],
-                ),
-                SizedBox(height: 20,), // space between bullet points
-                Row(
-                  children: [
-                    Icon(Icons.check, color: Colors.greenAccent,),
-                    SizedBox(width: 10,),
-                    Text("Search and Filter Contacts",
-                    style: TextStyle(fontSize: 20),),
-                  ],
-                ),
-                SizedBox(height: 20,),
-                Row(
-                  children: [
-                    Icon(Icons.check, color: Colors.greenAccent,),
-                    SizedBox(width: 10,),
-                    Text("Customize your settings",
-                    style: TextStyle(fontSize: 20),),
-                  ],
-                ),
-              ],
-            ),
-          ),
-
-          ),
-        ],
-      ),
+      body:IndexedStack( children: [
+        HomePageBody(),
+        ContactsPage(),
+        Container()
+      ],index: _selectedIndex,),
 
       bottomNavigationBar: BottomNavigationBar(
-        onTap: _navigateBottomBar,
+        onTap: (index) =>_navigateBottomBar(index),
         currentIndex: _selectedIndex,
           items: [
         BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
@@ -159,6 +58,130 @@ class _HomePageState extends State<HomePage> {
 
 
 
+
+
+    );
+  }
+}
+
+class HomePageBody extends StatelessWidget {
+  const HomePageBody({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+
+      children: [
+
+        Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+        child:Container(
+          width: double.infinity,
+          height: 200,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+          ),
+
+          padding: EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+                    Text("Quick Start",
+                      style: TextStyle(fontSize: 25),
+                    ),
+              SizedBox(height: 10,),
+
+              Text("Manage your contacts efficiently with our address book app",
+              style: TextStyle(fontSize: 18),),
+              SizedBox(height: 15,),
+
+              Container(
+                height: 35,
+                width: 200,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Padding(padding:
+                EdgeInsets.symmetric( horizontal: 12),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(Icons.add,
+                      color: Colors.black,),
+
+                    Text("Add Contacts",
+                    style: TextStyle(color: Colors.black,
+                    fontSize: 16
+                    ),
+                    ),
+                  ],
+                ),
+                ),
+
+              )
+
+
+            ],
+          ),
+
+
+
+        ),
+        ),
+        SizedBox(height: 100,),
+        // features box
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20) ,
+        child: Container(
+          width: double.infinity,
+          height: 200,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+          ),
+
+         padding: EdgeInsets.all(16),//in the container
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start, // children to the side
+            children: [
+              // create n manage contacts
+              Row(
+                children: [
+                  Icon(Icons.check, color: Colors.greenAccent,),
+                  SizedBox(width: 10,) ,// space between the the icon and text
+                  Text("Create and Manage Contacts",
+                  style: TextStyle(fontSize: 20),),
+                ],
+              ),
+              SizedBox(height: 20,), // space between bullet points
+              Row(
+                children: [
+                  Icon(Icons.check, color: Colors.greenAccent,),
+                  SizedBox(width: 10,),
+                  Text("Search and Filter Contacts",
+                  style: TextStyle(fontSize: 20),),
+                ],
+              ),
+              SizedBox(height: 20,),
+              Row(
+                children: [
+                  Icon(Icons.check, color: Colors.greenAccent,),
+                  SizedBox(width: 10,),
+                  Text("Customize your settings",
+                  style: TextStyle(fontSize: 20),),
+                ],
+              ),
+            ],
+          ),
+        ),
+
+        ),
+      ],
     );
   }
 }
